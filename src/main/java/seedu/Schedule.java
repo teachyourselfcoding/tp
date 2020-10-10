9cff8fc23a991194a83f201d2975656bdee125d9
t170815518 <christang_1023@outlook.com>
2020-10-10 12:09:08 +0800

    Add schedule class

---
+++ src/main/java/seedu/Schedule.java
@@ -0,0 +1,32 @@
+package java.seedu;
+import java.time.LocalDate;
+import java.seedu.Exception.InvalidRangeException;
+
+/**
+ * Class for schedule management.
+ */
+public class Schedule {
+
+	/**
+	 * Displays tasks on the days within the range.
+	 * @param startDay the start of the range
+	 * @param endDay the end of the range
+	 * @throws InvalidRangeException if startDay and endDay gives wrong range (e.g. endDay < startDay)
+	 * FIXME
+	 *  - add code and output based on UG
+	 *  - handle the task with frequency!
+	 */
+	void display(LocalDate startDay, LocalDate endDay) throws InvalidRangeException{
+
+	}
+
+	/**
+	 * Displays today's tasks.
+	 * FIXME
+	 *  - add code and output based on UG
+	 *  - handle the task with frequency!
+	 */
+	void display() {
+
+	}
+}
