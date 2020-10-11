672e379e006501dc496096d9cbc568ebbbcc7d61
Chun Yang <acyang97@gmail.com>
2020-10-11 16:27:10 +0800

    just for testing purposes

---
+++ src/main/java/seedu/duequest/DueQuest1.java
@@ -0,0 +1,16 @@
+package seedu.duequest;
+
+import seedu.*;
+import seedu.command.Command;
+import seedu.task.TaskList;
+import java.io.FileNotFoundException;
+
+/**
+ * Class just for codetesting purposes for our ScheduleManager
+ */
+public class DueQuest1 {
+    public static void main(String[] args) {
+        ScheduleManager scheduleManager = new ScheduleManager();
+        ModuleManager moduleManager = new ModuleManager();
+    }
+}
