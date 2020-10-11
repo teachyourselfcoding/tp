0621843a7b2d741a182044f306631e1b193e5350
Chun Yang <acyang97@gmail.com>
2020-10-11 19:36:41 +0800

    Just did some testing

--- src/main/java/seedu/duequest/DueQuest1.java
+++ src/main/java/seedu/duequest/DueQuest1.java
@@ -7,10 +7,33 @@ import java.io.FileNotFoundException;
 
 /**
  * Class just for codetesting purposes for our ScheduleManager
+ * Handle the UI, Storage later. Try to make the other commands work first.
  */
 public class DueQuest1 {
+    ScheduleManager scheduleManager = new ScheduleManager();
+    ModuleManager moduleManager = new ModuleManager();
+    Ui ui = new Ui();
+
+    public void run() {
+        boolean isExit = false;
+
+        while (!isExit) {
+            String fullCommand = ui.readCommand();
+            try {
+                Command c = Parser.parse(fullCommand);
+                c.execute(scheduleManager, moduleManager, ui);
+                isExit = c.isExit();
+            } catch (DueQuestException e) {
+                ui.showError(e.getExceptionType());
+            }
+        }
+        ui.byeMessage();
+    }
+
+    /**
+     * The main function to run the whole DueQuest app
+     */
     public static void main(String[] args) {
-        ScheduleManager scheduleManager = new ScheduleManager();
-        ModuleManager moduleManager = new ModuleManager();
+        new DueQuest1().run();
     }
 }
