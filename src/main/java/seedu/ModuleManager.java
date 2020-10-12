05786c92e9fa3fb594c61b1413232fba6cf2bab4
LowRiHao <haroic1997@gmail.com>
2020-10-12 12:05:41 +0800

    Added AddTaskToModule and Display methods

--- src/main/java/seedu/ModuleManager.java
+++ src/main/java/seedu/ModuleManager.java
@@ -1,5 +1,9 @@
 package seedu;
 
+import seedu.task.Deadline;
+import seedu.task.Task;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,13 +13,81 @@ import java.util.List;
  * into the ModuleManager as well according to the Module Code
  */
 public class ModuleManager {
-    List<Module> listOfModules;
+    ArrayList<Module> listOfModules;
 
     public ModuleManager() {
         this.listOfModules = new ArrayList<>();
     }
 
-    public ModuleManager(List<Module> listOfModules) {
+    public ModuleManager(ArrayList<Module> listOfModules) {
         this.listOfModules = listOfModules;
+    }
+
+    public void addModule(Module module){
+        listOfModules.add(module);
+    }
+
+    public Module getModule(String moduleCode) throws DueQuestException{
+        for (Module m:listOfModules) {
+            if (m.getModuleCode()==moduleCode){
+                return m;
+            }
+        }
+        throw new DueQuestException(DueQuestExceptionType.MISSING_MODULE);
+    }
+    public int getModuleIndex(String moduleCode) throws DueQuestException{
+        int indexCount =0;
+        for (Module m:listOfModules) {
+            if (m.getModuleCode()==moduleCode){
+                return indexCount;
+            }
+            else{
+                indexCount++;
+            }
+        }
+        throw new DueQuestException(DueQuestExceptionType.MISSING_MODULE);
+    }
+
+
+     public void addTaskToModule(String moduleCode, Task task, LocalDate date) throws DueQuestException{
+
+         try {
+             listOfModules.get(getModuleIndex(moduleCode)).addTask(task);
+             ScheduleManager.updateSchedule(date,task);
+         } catch (DueQuestException e) {
+             throw new DueQuestException(DueQuestExceptionType.MISSING_MODULE);
+         }
+     }
+
+    // Display all the task in a module
+    public void display(String moduleCode) {
+        for (Module m:listOfModules) {
+            if (m.getModuleCode().equals(moduleCode)) {
+               ArrayList<Task> tasks = m.getListOfTasks();
+               Ui.print("The list of task in " + moduleCode + ":");
+               Ui.printListGenericType(tasks);
+               Ui.showDivider();
+               break;
+            }
+        }
+    }
+    // Display all the task in a module on a specific date
+    public void display(String moduleCode, LocalDate date) {
+        for (Module m:listOfModules) {
+            if (m.getModuleCode().equals(moduleCode)) {
+                ArrayList<Task> filteredTasks = new ArrayList<>();
+                for (Task t: m.getListOfTasks()) {
+                    if (t instanceof Deadline){
+                        if (((Deadline) t).getDate().isEqual(date)){
+                            filteredTasks.add(t);
+                        }
+                    }
+                }
+                Ui.print("The list of task in " + moduleCode +" on " +date.toString() +" :");
+                Ui.printListGenericType(filteredTasks);
+                Ui.showDivider();
+
+            }
+        }
     }
 }
