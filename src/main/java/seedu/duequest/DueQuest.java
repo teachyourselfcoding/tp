6f8b973fae7548ee705dfd9d2991a93f0d8891c7
Chun Yang <acyang97@gmail.com>
2020-10-14 04:20:55 +0800

    commented out the preloadData methods. Now can try checking without adding in data

--- src/main/java/seedu/duequest/DueQuest.java
+++ src/main/java/seedu/duequest/DueQuest.java
@@ -7,7 +7,6 @@ import seedu.command.Command;
 import seedu.task.Deadline;
 import seedu.task.Lesson;
 import seedu.task.TaskList;
-import java.io.FileNotFoundException;
 import java.time.LocalDate;
 
 /**
@@ -74,9 +73,9 @@ public class DueQuest {
      */
     public void runn() {
         boolean isExit = false;
-        preloadData();
-        predeleteData();
-        testDisplayFunction();// used to test display function since add functionality not yet done
+        //preloadData();
+        //predeleteData();
+        //testDisplayFunction();// used to test display function since add functionality not yet done
 
         while (!isExit) {
             String fullCommand = ui.readCommand();
@@ -105,13 +104,13 @@ public class DueQuest {
      * use this to preload a set of data since our storage function not available yet.
      * you can use this for Edit, Delete, add...
      */
-
     public void predeleteData(){
         scheduleManager.deleteTask("v1.0");
     }
+
     public void preloadData(){
-        moduleManager.addModule(new seedu.Module("CS2113"));
-        moduleManager.addModule(new seedu.Module("EE2026"));
+        moduleManager.addModule(new Module("CS2113"));
+        moduleManager.addModule(new Module("EE2026"));
         moduleManager.addModule(new Module("CS1231"));
         try {
             moduleManager.addTaskToModule("CS2113",new Deadline("v1.0","2021-04-12"),LocalDate.parse("2021-04-12"));
